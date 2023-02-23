c53e6312dc41aed8151315986be17108d8071574
Nvsblmike <soundboylesh77@gmail.com>
2023-02-23 20:14:23 +0100

    Created whitelist app

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,31 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  /**
+   * A contractfactory in ethers.js is an abstraction used to deploy smart contracts
+   * so whitelistContract here is a factory for instances of our whitelist contract.
+   */
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  //here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  //10 is the max number of whitelisted addresses allowed
 
-  await lock.deployed();
+  //wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  //print the address of the deployed contract
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+main()
+.then(() => process.exit(0))
+.catch((error) => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
